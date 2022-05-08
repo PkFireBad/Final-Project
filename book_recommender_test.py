0d099f8f41ff01ffb8cc169f67ece3be2b2588d6
PkFireBad <83625260+PkFireBad@users.noreply.github.com>
2022-05-08 15:15:26 -0400

    Update book_recommender_test.py

--- book_recommender_test.py
+++ book_recommender_test.py
@@ -1,8 +1,16 @@
-import book_recommender
+from book_recommender import Recommender, Book
 
 def test_book_build():
-    pass
+    book1 = Book("Harry Potter and the Half-Blood Prince (Harry Potter #6)", "J.K. Rowling", 4.57, "long")
+    assert(book1.compare("J.K. Rowling", 5, "long") == 3)
 
 def test_recommender_build():
-    pass
-    
+    recommender = Recommender()
+    recommendation1 = recommender.recommendation("J.K. Rowling", 5, "long")
+    assert(recommendation1 != None)
+    assert (len(recommendation1) > 0)
+    assert(recommendation1[0].title == "Harry Potter and the Half-Blood Prince (Harry Potter #6)")
+
+if __name__ == "__main__":
+    test_book_build()
+    test_recommender_build()
